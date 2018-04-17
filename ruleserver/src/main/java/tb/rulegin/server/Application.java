89ae966b61c63855acd714849752e7b5a0ac6e8b
JaryZhen <jary@JaryZhendeMacBook-Pro.local>
2018-04-17 11:58:55 +0800

    去掉本地仓库配置

--- ruleserver/src/main/java/tb/rulegin/server/Application.java
+++ ruleserver/src/main/java/tb/rulegin/server/Application.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
-@ComponentScan({"cn.neucloud.server"})
+@ComponentScan({"tb.rulegin.server"})
 public class Application {
 
     public static void main(String[] args) {
