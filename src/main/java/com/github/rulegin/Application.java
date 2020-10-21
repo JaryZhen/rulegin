e9ac922b14faa09c77a2532ce9c452f9054d7b13
JaryZhen <zhenguoyou@vipkid.com.cn>
2020-10-21 11:19:58 +0800

    update config

--- src/main/java/com/github/rulegin/Application.java
+++ src/main/java/com/github/rulegin/Application.java
@@ -4,12 +4,11 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication()
 @ComponentScan({"com.github.rulegin"})
 public class Application {
 
@@ -22,13 +21,13 @@ public class Application {
     private Environment env;
 
 
-    @Bean(destroyMethod =  "close")
+    @Bean(destroyMethod = "close")
     public DruidDataSource dataSource() {
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setUrl(env.getProperty("spring.datasource.url"));
         dataSource.setUsername(env.getProperty("spring.datasource.username"));
         dataSource.setPassword(env.getProperty("spring.datasource.password"));
-        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
         dataSource.setInitialSize(2);
         dataSource.setMaxActive(20);
         dataSource.setMinIdle(0);
--- src/main/java/com/github/rulegin/InstallApplication.java
+++ src/main/java/com/github/rulegin/InstallApplication.java
@@ -1,26 +1,8 @@
-/**
- * Copyright © 2016-2017 The Thingsboard Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.github.rulegin;
 
 import com.github.rulegin.actors.install.RuleginInstallService;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -30,7 +12,7 @@ import java.util.Arrays;
 
 @EnableAsync
 @EnableSwagger2
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication()
 @ComponentScan({"com.gtihub.rulegin"})
 public class InstallApplication {
 
